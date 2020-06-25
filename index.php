a6b175ae1927903535825db69cc182d68302267c
python-the-snake <dmitriy-shalimov@yandex.ru>
2020-06-25 12:28:50 +0300

    php storm 1

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
     echo 'Hello world';
+
+    $my_variable = 100;
+    $my_new_variable = 200;
+
+    echo $my_variable;
+
+    echo '<hr>';
+
+    echo $my_new_variable;
+
+    // data types in php: integer, float, string, array
+
+    $array = array(1, 2, 3, 4, 5);
+
+    echo $array[];
 ?>
